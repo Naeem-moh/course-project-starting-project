0ec44629ed49e8d5b891a9fb26494478fcd0442a
naeem-moh <mohammed.naeem@gmail.com>
2024-08-18 12:02:15 +0300

    merging local repos

--- src/app/router/routing.module.ts
+++ src/app/router/routing.module.ts
@@ -5,11 +5,11 @@ import { ShoppingListComponent } from '../shopping-list/shopping-list.component'
 import { RecipeDetailComponent } from '../recipes/recipe-detail/recipe-detail.component';
 import { RecipeDetailsDefaultComponent } from '../recipes/recipe-details-default/recipe-details-default.component';
 import { RecipeEditComponent } from '../recipes/recipe-edit/recipe-edit.component';
-import { HttpTestComponent } from '../http-test/http-test.component';
+//import { HttpTestComponent } from '../http-test/http-test.component';
 import { RecipesResolver } from '../recipes/recipes.resolver';
 
 const routerConfig: Routes = [
-  { path: 'test', component: HttpTestComponent },
+  //{ path: 'test', component: HttpTestComponent },
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   {
     path: 'recipes',
